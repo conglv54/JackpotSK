4bf16c3a43bd2af3e980de23cf53c145c170115b
le cong <conglv54@gmail.com>
2014-11-28 01:10:32 +0700

    Initial Commit

---
+++ JackPotSK/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  JackPotSK
+//
+
+//  Copyright (c) 2014 Lê Công. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ JackPotSK/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  JackPotSK
+//
+
+//  Copyright (c) 2014 Lê Công. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
